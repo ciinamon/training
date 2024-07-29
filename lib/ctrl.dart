406fa271363fae5e4395140ad3bf96d12624e1ff
felicia <rievaldafellisa@gmail.com>
2024-07-29 21:53:02 +0700

    username list page

--- lib/ctrl.dart
+++ lib/ctrl.dart
@@ -3,3 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 Future<void> createDocument(Map<String, dynamic> data) async {
   await FirebaseFirestore.instance.collection('username').doc().set(data);
 }
+
+Future<QuerySnapshot<Map<String, dynamic>>> getColl() async {
+  var getColl = await FirebaseFirestore.instance.collection('username').get();
+  return getColl;
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:training/ctrl.dart';
 import 'package:training/input_page.dart';
 
 class Home extends StatelessWidget {
@@ -22,42 +22,65 @@ class Home extends StatelessWidget {
         },
         child: const Icon(Icons.add),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            OutlinedButton(
-                onPressed: () async {
-                  await FirebaseFirestore.instance.collection('username').doc('abc').set({'nama': 'fellisa'});
-                },
-                child: const Text('C')),
-            const SizedBox(height: 20),
-            OutlinedButton(
-                onPressed: () async {
-                  await FirebaseFirestore.instance.collection('username').get();
-                },
-                child: const Text('R')),
-            const SizedBox(height: 20),
-            OutlinedButton(
-                onPressed: () async {
-                  await FirebaseFirestore.instance.collection('username').doc('abc').get();
-                },
-                child: const Text('R Document')),
-            const SizedBox(height: 20),
-            OutlinedButton(
-                onPressed: () async {
-                  await FirebaseFirestore.instance.collection('username').doc('abc').update({'nama': 'fellisa new'});
-                },
-                child: const Text('U')),
-            const SizedBox(height: 20),
-            OutlinedButton(
-                onPressed: () async {
-                  await FirebaseFirestore.instance.collection('username').doc('abc').delete();
-                },
-                child: const Text('D')),
-            const SizedBox(height: 20),
-          ],
-        ),
+      body: Column(
+        children: [
+          FutureBuilder(
+            future: getColl(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.hasData) {
+                return Column(
+                  children: [
+                    ...List.generate(
+                      snapshot.data!.docs.length,
+                      (index) {
+                        return Card(
+                          child: ListTile(
+                            title: Text((snapshot.data!.docs[index].data()['name'])),
+                            subtitle: Text(snapshot.data!.docs[index].id),
+                          ),
+                        );
+                      },
+                    )
+                  ],
+                );
+              }
+              return const Text('any');
+            },
+          )
+          // OutlinedButton(
+          //     onPressed: () async {
+          //       await FirebaseFirestore.instance.collection('username').doc('abc').set({'nama': 'fellisa'});
+          //     },
+          //     child: const Text('C')),
+          // const SizedBox(height: 20),
+          // OutlinedButton(
+          //     onPressed: () async {
+          //       await FirebaseFirestore.instance.collection('username').get();
+          //     },
+          //     child: const Text('R')),
+          // const SizedBox(height: 20),
+          // OutlinedButton(
+          //     onPressed: () async {
+          //       await FirebaseFirestore.instance.collection('username').doc('abc').get();
+          //     },
+          //     child: const Text('R Document')),
+          // const SizedBox(height: 20),
+          // OutlinedButton(
+          //     onPressed: () async {
+          //       await FirebaseFirestore.instance.collection('username').doc('abc').update({'nama': 'fellisa new'});
+          //     },
+          //     child: const Text('U')),
+          // const SizedBox(height: 20),
+          // OutlinedButton(
+          //     onPressed: () async {
+          //       await FirebaseFirestore.instance.collection('username').doc('abc').delete();
+          //     },
+          //     child: const Text('D')),
+          // const SizedBox(height: 20),
+        ],
       ),
     );
   }
